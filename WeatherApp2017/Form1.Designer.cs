1f9a44abd3e58419d8d3be4f28bcc19454f3cb4c
Syuuta Shibuya <7280ayubihs@gmail.com>
2017-09-04 21:49:42 +0900

    最初のコミット

--- WeatherApp2017/Form1.Designer.cs
+++ WeatherApp2017/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -43,22 +45,37 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "新宿",
+            "八王子",
+            "水戸"});
             this.comboBox1.Location = new System.Drawing.Point(151, 11);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 20);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(259, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dateTimePicker1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +84,7 @@
 
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- WeatherApp2017/Form1.cs
+++ WeatherApp2017/Form1.cs
@@ -12,6 +12,8 @@ namespace WeatherApp2017
 {
     public partial class Form1 : Form
     {
+        System.Random random;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +21,28 @@ namespace WeatherApp2017
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            random = new System.Random();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            Image image = Properties.Resources.sunny;
+            switch (random.Next(0, 3))
+            {
+                case 0:
+                    image = Properties.Resources.sunny;
+                    break;
+                case 1:
+                    image = Properties.Resources.cloudy;
+                    break;
+                case 2:
+                    image = Properties.Resources.raining;
+                    break;
+                case 3:
+                    image = Properties.Resources.snowy;
+                    break;
+            }
+            pictureBox1.Image = image;
         }
     }
 }
