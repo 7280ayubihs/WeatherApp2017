f849daa972da3502c2474b677981eefa2da12ca7
Syuuta Shibuya <7280ayubihs@gmail.com>
2017-09-18 13:06:47 +0900

    デモ画面にDBサーバ連携機能を追加

--- WeatherApp2017/Form1.cs
+++ WeatherApp2017/Form1.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace WeatherApp2017
 {
     public partial class Form1 : Form
     {
+        private string pythonExePath = "python.exe";
+        private string pythonScriptPath = "python.py";
+
         public Form1()
         {
             InitializeComponent();
@@ -16,6 +22,29 @@ namespace WeatherApp2017
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // 設定ファイルをロード
+            List<string> pointItems = new List<string>();
+            using (StreamReader sr = new StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\div22.json"))
+            {
+                JObject jobj = JObject.Parse(sr.ReadToEnd()); ;
+
+                // pyhton.exeのパス
+                pythonExePath = jobj["Python"]["Exe"].ToString();
+
+                // 実行するpythonスクリプトのパス
+                pythonScriptPath = jobj["Python"]["Scripy"].ToString();
+
+                // データ蓄積サーバの get_sensor_data のベースUri
+                UriGenerator.SetBaseUriGetSensorData(jobj["Uri"]["GetSensorData"].ToString());
+
+                // 地点コンボボックスの初期設定
+                for (int i = 0; i < jobj["Point"]["Items"].Count(); i++)
+                {
+                    comboBoxPointSelect.Items.Add(jobj["Point"]["Items"][i].ToString());
+                }
+                comboBoxPointSelect.SelectedItem = jobj["Point"]["Initial"].ToString();
+            }
+
             // コンボボックスの初期設定
             comboBoxTimeSelect.Items.AddRange(new string[] {
                 " 0時",
@@ -28,21 +57,47 @@ namespace WeatherApp2017
                 "21時" }
             );
             comboBoxTimeSelect.SelectedIndex = 0;
+
+            // 予想日コンボボックスの初期設定
+            comboBoxExpectedDateSelect.Items.AddRange(new string[] {
+                "翌日",
+                "今日"
+            });
+            comboBoxExpectedDateSelect.SelectedIndex = 0;
         }
 
         private void buttonTest_Click(object sender, EventArgs e)
         {
-            // 地点A（Tokyo）
-            JObject jObjectA = JObject.Parse(new HttpClient().GetStringAsync("http://weather.livedoor.com/forecast/webservice/json/v1?city=130010").Result);
-            pictureBoxPointA.Image = getWeatherBitmap((string)(jObjectA["forecasts"][0]["telop"] as JValue).Value);
+            // 正解データを Weather Hacks から取得して表示
+            string uriWeatherHacks = UriGenerator.GenerateWeatherHacksUri("東京");
+            JObject jsonWeatherHacks = JObject.Parse(new HttpClient().GetStringAsync(uriWeatherHacks).Result);
+            pictureBoxActual.Image = GetWeatherBitmap((string)(jsonWeatherHacks["forecasts"][0]["telop"] as JValue).Value);
 
-            // 地点B（Yokohama）
-            JObject jObjectB = JObject.Parse(new HttpClient().GetStringAsync("http://weather.livedoor.com/forecast/webservice/json/v1?city=140010").Result);
-            pictureBoxPointB.Image = getWeatherBitmap((string)(jObjectA["forecasts"][0]["telop"] as JValue).Value);
+            // サーバに問い合わせて、jsonデータを取得
+            string uriGetSensorData = UriGenerator.GenerateGetSensorDataUri(
+                dateTimePicker.Value.Year,
+                dateTimePicker.Value.Month, 
+                dateTimePicker.Value.Day,
+                comboBoxTimeSelect.SelectedIndex * 3);
+            JArray jsonGetSensorData = JArray.Parse(new HttpClient().GetStringAsync(uriGetSensorData).Result);
 
-            // 地点C（Mito）
-            JObject jObjectC = JObject.Parse(new HttpClient().GetStringAsync("http://weather.livedoor.com/forecast/webservice/json/v1?city=080010").Result);
-            pictureBoxPointC.Image = getWeatherBitmap((string)(jObjectA["forecasts"][0]["telop"] as JValue).Value);
+            // 一旦、ファイルに保存
+            File.WriteAllText(
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "tokyo_weather.txt"),
+                jsonGetSensorData.ToString());
+
+            // DataGridViewにjsonデータを表示
+            //TODO: 
+
+            // Python 実行して、終了まで待機
+            Process ps = new Process();
+            ps.StartInfo.FileName = pythonExePath;
+            ps.StartInfo.Arguments = pythonScriptPath;
+            ps.Start();
+            ps.WaitForExit();
+
+            // 終了したら、画面に結果を表示
+            pictureBoxExpect.Image = GetWeatherBitmap(ps.ExitCode);
         }
 
         /// <summary>
@@ -50,7 +105,7 @@ namespace WeatherApp2017
         /// </summary>
         /// <param name="weatherStr">天気を示す文字列</param>
         /// <returns>対応する画像</returns>
-        private Bitmap getWeatherBitmap(string weatherStr)
+        private Bitmap GetWeatherBitmap(string weatherStr)
         {
             Bitmap bitmap; ;
             switch (weatherStr.Substring(0, 1))
@@ -74,14 +129,33 @@ namespace WeatherApp2017
             return bitmap;
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        /// <summary>
+        /// 天気を示す文字列から画面に表示する画像を取得します。
+        /// </summary>
+        /// <param name="weatherStr">天気を示す文字列</param>
+        /// <returns>対応する画像</returns>
+        private Bitmap GetWeatherBitmap(int weatherCode)
         {
-
-        }
-
-        private void textBox14_TextChanged(object sender, EventArgs e)
-        {
-
+            Bitmap bitmap; ;
+            switch (weatherCode)
+            {
+                case 0:
+                    bitmap = Properties.Resources.sunny;
+                    break;
+                case 1:
+                    bitmap = Properties.Resources.cloudy;
+                    break;
+                case 2:
+                    bitmap = Properties.Resources.raining;
+                    break;
+                case 3:
+                    bitmap = Properties.Resources.snowy;
+                    break;
+                default:
+                    bitmap = Properties.Resources.cloudy;
+                    break;
+            }
+            return bitmap;
         }
     }
 }
---
+++ WeatherApp2017/UriGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp2017
+{
+    class UriGenerator
+    {
+        private static string mBaseUriGetSensorData = null;
+
+        public static void SetBaseUriGetSensorData(string baseUri)
+        {
+            mBaseUriGetSensorData = baseUri;
+        }
+
+        public static string GenerateGetSensorDataUri(int year, int month, int day, int hour)
+        {
+            return String.Format(mBaseUriGetSensorData, year, month, day, hour);
+        }
+
+        public static string GenerateWeatherHacksUri(string city)
+        {
+            return String.Format(
+                "http://weather.livedoor.com/forecast/webservice/json/v1?city={0}",
+                GetWeatherHacksCityCode(city));
+        }
+
+        private static int GetWeatherHacksCityCode(string city)
+        {
+            switch (city)
+            {
+                // 関東地方のみ
+                case "水戸": return 80010;
+                case "土浦": return 80020;
+                case "宇都宮": return 90010;
+                case "大田原": return 90020;
+                case "前橋": return 100010;
+                case "みなかみ": return 100020;
+                case "さいたま": return 110010;
+                case "熊谷": return 110020;
+                case "秩父": return 110030;
+                case "千葉": return 120010;
+                case "銚子": return 120020;
+                case "館山": return 120030;
+                case "東京": return 130010;
+                case "大島": return 130020;
+                case "八丈島": return 130030;
+                case "父島": return 130040;
+                case "横浜": return 140010;
+                case "小田原": return 140020;
+            }
+            // デフォルトは東京
+            return 130010;
+        }
+    }
+}
